2793afa82c72f8c30d7ba9a7ab13d1dda53ef05c
Davide Libenzi <davide.libenzi@gmail.com>
2023-12-24 07:29:13 +0100

    License migration.

--- rconsole/rconsole/wce420inc/pkfuncs.h
+++ rconsole/rconsole/wce420inc/pkfuncs.h
@@ -1,13 +1,17 @@
-//
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//
-//
-// Use of this source code is subject to the terms of the Microsoft end-user
-// license agreement (EULA) under which you licensed this SOFTWARE PRODUCT.
-// If you did not accept the terms of the EULA, you are not authorized to use
-// this source code. For a copy of the EULA, please see the LICENSE.RTF on your
-// install media.
-//
+//    Copyright 2023 Davide Libenzi
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
 /*++
 THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
 ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
